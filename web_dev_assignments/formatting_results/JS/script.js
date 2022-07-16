9f1f592198cb701ad766cc6c2d599bda90674e11
Joseph W. DeWeese <52636579+rangerboy901@users.noreply.github.com>
2022-07-16 16:03:55 -0500

    initial

---
+++ web_dev_assignments/formatting_results/JS/script.js
@@ -0,0 +1,13 @@
+/*
+Joseph DeWeese
+16 July 2022
+Formatting Results
+ */
+
+//  calculate area of a triangle based upon user input
+
+//Prompt user
+
+let height = parseFloat(prompt("What is the height of your rectangle?","EX: 7."))
+//parse to correct data types
+
